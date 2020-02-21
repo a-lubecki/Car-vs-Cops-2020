ac5035bf256b78e0db09b6886dac302f23d55007
Aurel <aurel.lubecki@gmail.com>
2020-02-21 21:41:10 +0100

    Added FX for combo

--- Ketchapp/Assets/CarVsCops/Scripts/UI/UIComboBehavior.cs
+++ Ketchapp/Assets/CarVsCops/Scripts/UI/UIComboBehavior.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
 
@@ -6,10 +7,16 @@ using DG.Tweening;
 public class UIComboBehavior : BaseUIBehavior {
 
 
+    [SerializeField] private RawImage imageFX = null;
     [SerializeField] private TextMeshProUGUI textMultiplier = null;
+
+    [SerializeField] private Color colorFX0 = Color.white;
+    [SerializeField] private Color colorFX1 = Color.white;
+
     [SerializeField] private float multiplierAnimationScale = 1;
     [SerializeField] private Color multiplierAnimationColor = Color.white;
 
+    private Color currentColor;
     private Color originalTextMultiplierColor;
 
 
@@ -17,6 +24,24 @@ public class UIComboBehavior : BaseUIBehavior {
         base.Awake();
 
         originalTextMultiplierColor = textMultiplier.color;
+    }
+
+    protected override void UpdateUI(bool animated) {
+        base.UpdateUI(animated);
+
+        imageFX.enabled = IsShown;
+
+        if (IsShown) {
+            PlayFXColorChange();
+        }
+    }
+
+    private void PlayFXColorChange() {
+
+        imageFX.DOKill(false);
+
+        imageFX.color = colorFX0;
+        imageFX.DOColor(colorFX1, 0.6f).SetLoops(-1, LoopType.Yoyo);
     }
 
     public void UpdateMultiplier(int multiplier, bool animated) {
